c73889c12ca2a1e640961831c2b0de9729323855
Cécile <cecile@simula.no>
2022-10-27 14:43:29 +0200

    Add networks meshing features

---
+++ src/networks_fenicsx/mesh/mesh.py
@@ -0,0 +1,175 @@
+import networkx as nx
+import numpy as np
+
+import ufl
+from mpi4py import MPI
+from dolfinx import fem, io, mesh
+from dolfinx import cpp as _cpp
+
+import gmsh
+
+# from utils import timeit
+
+'''
+The Graphnics class constructs fenics meshes from networkx directed graphs.
+
+'''
+
+class NetworkGraph(nx.DiGraph):
+    '''
+    Make FEniCSx mesh from networkx directed graph
+    '''
+
+    def __init__(self):
+        nx.DiGraph.__init__(self)
+
+        self.comm = MPI.COMM_WORLD
+
+        self.bifurcation_ixs = []
+        self.boundary_ixs = []
+
+        self.msh = None
+        self.subdomains = None
+        self.boundaries = None
+        self.global_tangent = None
+
+        self.BIF_IN = 1
+        self.BIF_OUT = 2
+        self.BOUN_IN = 3
+        self.BOUN_OUT = 4
+
+
+    def build_mesh(self, lcar=1, export=False):
+        '''
+        Makes a fenics mesh from the graph
+        
+        Args:
+            lcar (float): Characteristic length of the elements
+        Returns:
+            mesh : the global mesh
+        
+        '''
+
+        self.geom_dim = len(self.nodes[1]['pos'])
+        self.num_edges = len(self.edges)
+
+        domain = ufl.Mesh(ufl.VectorElement("Lagrange", "interval", 1))
+        vertex_coords = np.asarray( [self.nodes[v]['pos'] for v in self.nodes()  ] )
+        cells_array = np.asarray( [ [u, v] for u,v in self.edges() ] )
+        
+        gmsh.initialize()
+        model = gmsh.model()
+        pts = []
+        lines = []
+        for i,v in enumerate(vertex_coords):
+            pts.append(gmsh.model.geo.addPoint(v[0], v[1], v[2], lcar))
+
+        for i,c in enumerate(cells_array):
+            lines.append(gmsh.model.geo.addLine(pts[c[0]], pts[c[1]]))
+
+        gmsh.model.geo.synchronize()
+        for i,line in enumerate(lines):
+            gmsh.model.addPhysicalGroup(1, [line], i)
+        
+        gmsh.model.mesh.generate(1)
+        
+        self.msh, self.subdomains, self.boundaries = io.gmshio.model_to_mesh(
+            gmsh.model, comm=MPI.COMM_WORLD, rank=0, gdim=3)
+        gmsh.finalize()
+
+        if export:
+            with io.XDMFFile(self.comm, "XDMF/mesh.xdmf", "w") as file:
+                file.write_mesh(self.msh)
+                file.write_meshtags(self.subdomains)
+
+
+    def build_network_submeshes(self):
+        
+        tdim = self.msh.topology.dim
+        gdim = self.msh.geometry.dim
+
+        DG0 = fem.VectorFunctionSpace(self.msh, ("DG", 0), dim=gdim)
+        self.global_tangent = fem.Function(DG0)
+
+        for i, (u,v) in enumerate(self.edges):
+            edge_subdomain = self.subdomains.find(i)
+            
+            self.edges[u,v]['submesh'], self.edges[u,v]['entity_map'] = mesh.create_submesh(self.msh, tdim, edge_subdomain)[0:2]
+            submesh_points = self.edges[u,v]['submesh'].geometry.x
+            self.edges[u,v]['tag'] = i
+            
+            # Compute tangent
+            tangent = np.asarray(self.nodes[v]['pos'])-np.asarray(self.nodes[u]['pos'])
+            tangent *= 1/np.linalg.norm(tangent)
+            self.edges[u,v]['tangent'] = tangent
+            for cell in edge_subdomain:
+                self.global_tangent.x.array[gdim*cell:gdim*(cell+1)] = tangent
+
+            self.edges[u,v]["entities"] = []
+            self.edges[u,v]["b_values"] = []
+
+
+    def build_markers(self, export=False):
+        # Marking the bifurcations (in/out) and boundaries (in/out) for extermities of each edges
+        for n, v in enumerate(self.nodes()):
+            num_conn_edges = len(self.in_edges(v)) + len(self.out_edges(v))
+            if num_conn_edges == 0:
+                print(f'Node {v} in G is lonely (i.e. unconnected)')
+
+            bifurcation = bool(num_conn_edges > 1)
+            boundary = bool(num_conn_edges == 1)
+            if bifurcation:
+                self.bifurcation_ixs.append(v)
+            if boundary:
+                self.boundary_ixs.append(v)            
+            
+            for i,e in enumerate(self.in_edges(v)):
+                e_msh = self.edges[e]['submesh']
+                entities = mesh.locate_entities(e_msh,0,
+                                                lambda x: np.logical_and(np.isclose(x[0], self.nodes[v]['pos'][0]),
+                                                                         np.isclose(x[1], self.nodes[v]['pos'][1]),
+                                                                         np.isclose(x[2], self.nodes[v]['pos'][2])))
+                self.edges[e]["entities"].append(entities)
+                if bifurcation : 
+                    b_values_in = np.full(entities.shape, self.BIF_IN, np.intc)
+                elif boundary:
+                    b_values_in = np.full(entities.shape, self.BOUN_OUT, np.intc)
+                self.edges[e]["b_values"].append(b_values_in)
+                
+            for i,e in enumerate(self.out_edges(v)):
+                e_msh = self.edges[e]['submesh']
+                entities  = mesh.locate_entities(e_msh,0,
+                                                 lambda x: np.logical_and(np.isclose(x[0], self.nodes[v]['pos'][0]),
+                                                                          np.isclose(x[1], self.nodes[v]['pos'][1]),
+                                                                          np.isclose(x[2], self.nodes[v]['pos'][2])))
+                self.edges[e]["entities"].append(entities)
+                if bifurcation : 
+                    b_values_out = np.full(entities.shape, self.BIF_OUT, np.intc)
+                elif boundary:
+                    b_values_out = np.full(entities.shape, self.BOUN_IN, np.intc)
+                self.edges[e]["b_values"].append(b_values_out)
+
+        # Creating the edges meshtags
+        for i, e in enumerate(self.edges):
+            e_msh = self.edges[e]['submesh']
+            indices, pos = np.unique(np.hstack(self.edges[e]["entities"]), return_index=True)
+            self.edges[e]['vf'] = mesh.meshtags(e_msh, 0, indices, np.hstack(self.edges[e]["b_values"])[pos])
+            e_msh.topology.create_connectivity(0, 1)
+
+            if export:
+                with io.XDMFFile(self.comm, "XDMF/edge_" + str(i) + ".xdmf", "w") as file:
+                    file.write_mesh(e_msh)
+                    file.write_meshtags(self.edges[e]['vf'])
+
+    def mesh(self):
+        return self.msh
+    
+    def global_tangent(self, export=False):
+
+        if export:
+            self.global_tangent.x.scatter_forward()       
+            with io.XDMFFile(self.comm, "tangent.xdmf", "w") as file:
+                file.write_mesh(self.msh)
+                file.write_function(self.global_tangent)
+        
+        return self.global_tangent
---
+++ src/networks_fenicsx/mesh/mesh_generation.py
@@ -0,0 +1,82 @@
+from mesh import *
+
+def make_line_graph(n, export=False):
+    '''
+    Make a graph along the unit x-axis with n nodes
+    '''
+
+    G = NetworkGraph()
+
+    dx = 1/(n-1)
+    print("Adding nodes 0 to ", n)
+    G.add_nodes_from(range(0,n))
+    for i in range(0,n):
+        G.nodes[i]['pos']=[i*dx,0,0]
+    
+    for i in range(0,n-1):
+        print("Adding edge [", i, ", ", i+1, "]")
+        G.add_edge(i,i+1)
+
+    G.build_mesh(export=export)
+    G.build_network_submeshes()
+    G.build_markers(export=export)
+
+    return G
+    
+def make_Y_bifurcation(export=False):
+    '''
+    Make a 3 branches network with one bifurcation
+    '''
+
+    G = NetworkGraph()
+    
+    G.add_nodes_from([0, 1, 2, 3])
+    G.nodes[0]['pos']=[0,0,0]
+    G.nodes[1]['pos']=[0,0.5,0]
+    G.nodes[2]['pos']=[-0.5,1,0]
+    G.nodes[3]['pos']=[0.5,1,0]
+
+    G.add_edge(0,1)
+    G.add_edge(1,2)
+    G.add_edge(1,3)
+
+    G.build_mesh(export=export)
+    G.build_network_submeshes()
+    G.build_markers(export=export)
+
+    return G
+
+
+def make_double_Y_bifurcation(export=False):
+
+    G = NetworkGraph()
+
+    G.add_nodes_from([0, 1, 2, 3,4,5,6,7])
+    G.nodes[0]['pos']=[0,0,0]
+    G.nodes[1]['pos']=[0,0.5,0]
+    G.nodes[2]['pos']=[-0.5,1,0]
+    G.nodes[3]['pos']=[0.5,1,0]
+
+    G.add_edge(0,1)
+    G.add_edge(1,2)
+    G.add_edge(1,3)
+
+    G.nodes[4]['pos']=[-0.75,1.5,0]
+    G.nodes[5]['pos']=[-0.25,1.5,0]
+
+    G.nodes[6]['pos']=[0.25,1.5,0]
+    G.nodes[7]['pos']=[0.75,1.5,0]
+    
+    G.add_edge(2,4)
+    G.add_edge(2,5)
+    G.add_edge(3,6)
+    G.add_edge(3,7)
+
+    G.build_mesh(export=export)
+    G.build_network_submeshes()
+    G.build_markers(export=export)
+
+    return G     
+
+if __name__ == '__main__':
+    make_Y_bifurcation(export=True)
