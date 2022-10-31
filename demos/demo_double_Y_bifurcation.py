6aa584b1b679d611b3dcec51816eb677da5d200d
Cécile <cecile@simula.no>
2022-10-31 15:01:32 +0100

    Add demo

---
+++ demos/demo_double_Y_bifurcation.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+
+from networks_fenicsx.mesh import mesh_generation
+from networks_fenicsx.solver import assembly, solver
+from networks_fenicsx.config import Config
+
+
+# Clear fenics cache
+print('Clearing cache')
+os.system('dijitso clean')
+
+cfg = Config()
+cfg.outdir = "demo_double_Y_bifurcation"
+cfg.export = True
+cfg.clean = True
+
+# Create Y bifurcation graph
+G = mesh_generation.make_double_Y_bifurcation(cfg)
+
+
+class p_bc_expr:
+    def eval(self, x):
+        return np.full(x.shape[1], x[0])
+
+
+assembler = assembly.Assembler(cfg, G)
+assembler.compute_forms(p_bc_ex=p_bc_expr())
+assembler.assemble()
+
+solver = solver.Solver(cfg, G, assembler)
+(fluxes, pressure) = solver.solve()
