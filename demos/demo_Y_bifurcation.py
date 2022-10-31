dac6b9ef539d4e55773a8df6ebcafabd05d1eb4b
Cécile <cecile@simula.no>
2022-10-31 09:42:23 +0100

    More flake8 fixes

--- demos/demo_Y_bifurcation.py
+++ demos/demo_Y_bifurcation.py
@@ -7,14 +7,16 @@ from networks_fenicsx.solver import assembly, solver
 
 # Clear fenics cache
 print('Clearing cache')
-os.system('dijitso clean') 
+os.system('dijitso clean')
 
 # Create Y bifurcation graph
 G = mesh_generation.make_Y_bifurcation()
 
+
 class p_bc_expr:
     def eval(self, x):
         return np.full(x.shape[1], x[0])
+
 
 assembler = assembly.Assembler(G)
 assembler.compute_forms(p_bc_ex=p_bc_expr())
@@ -24,7 +26,7 @@ solver = solver.Solver(G, assembler)
 (fluxes, pressure) = solver.solve()
 
 # Write to file
-for i,q in enumerate(fluxes):
+for i, q in enumerate(fluxes):
     with io.XDMFFile(G.msh.comm, "demo_Y_bifurcation/flux_" + str(i) + ".xdmf", "w") as file:
         file.write_mesh(q.function_space.mesh)
         file.write_function(q)
